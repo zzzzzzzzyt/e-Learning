498fdeaf833203ac7a39756ba4ea4704e8446be3
asus <836585692@qq.com>
2022-06-13 14:17:46 +0800

    6.13

--- service/service_cms/src/main/java/com/atguigu/educms/CmsApplication.java
+++ service/service_cms/src/main/java/com/atguigu/educms/CmsApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @ComponentScan({"com.atguigu"}) //指定扫描位置
